c57b81665842a6c6d1ae1ad9c6e371c54eb043b8
Juan Manuel <juanma.otegui@gmail.com>
2020-07-18 21:27:02 -0300

    Updates

---
+++ precache-manifest.b975b83e6b216d92818727e2693d3fba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fc108860f808b3b21aa9837bb52cbcb",
+    "url": "/PersonalWebsite/index.html"
+  },
+  {
+    "revision": "aed3250a9751d5efd0ec",
+    "url": "/PersonalWebsite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cda409a99e5c1588bc53",
+    "url": "/PersonalWebsite/static/js/2.ba79406c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PersonalWebsite/static/js/2.ba79406c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aed3250a9751d5efd0ec",
+    "url": "/PersonalWebsite/static/js/main.34e3be1e.chunk.js"
+  },
+  {
+    "revision": "05a2b04fe66c16cd332d",
+    "url": "/PersonalWebsite/static/js/runtime-main.b2dd1bbe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PersonalWebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
